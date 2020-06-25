d0f9f1fd6390b4900b724660a3268a29c5879860
Anatoly Belikov <awbelikov@gmail.com>
2020-06-25 17:55:27 +0300

    refactoring

---
+++ util.py
@@ -0,0 +1,252 @@
+import sys
+import os
+import lzma
+import math
+import random
+import string
+from collections import defaultdict
+from typing import *
+
+import numpy
+import pandas
+
+from sklearn.metrics import recall_score, precision_score, f1_score, confusion_matrix
+
+
+def split_by_study(merged, bmc, study_name=None):
+    """
+    Split one study out for cross-validation
+
+    merged: pandas.DataFrame
+        Genes + treatments table
+    bmc: pandas.DataFrame
+        averaged treatment table
+    study_name: str
+        Optional param - process only singe study
+
+    """
+    for eval_study in set(bmc.study):
+        if study_name:
+            eval_study = study_name
+        print(eval_study)
+        bmc_train = bmc[bmc.study != eval_study]
+        bmc_val = bmc[bmc.study == eval_study]
+        assert (not set(bmc_train.patient_ID).intersection(set(bmc_val.patient_ID)))
+
+        train_split = merged[merged.patient_ID.isin(bmc_train.patient_ID)]
+        val_split = merged[merged.patient_ID.isin(bmc_val.patient_ID)]
+        assert val_split.patient_ID.to_list() == bmc_val.patient_ID.to_list()
+        train_data = train_split[feature_columns].to_numpy()
+        train_labels = train_split[label_columns].to_numpy().astype(int)
+        val_data = val_split[feature_columns].to_numpy()
+        val_labels = val_split[label_columns].to_numpy().astype(int)
+        yield train_data, train_labels, val_data, val_labels
+        if study_name:
+            break
+
+
+def select_balanced_idx(study, num):
+    if not num % 2 == 0:
+        num = num + 1
+    validation = []
+    pos_outcome = study[study.posOutcome == 1].patient_ID
+    neg_outcome = study[study.posOutcome == 0].patient_ID
+    pos_idx = numpy.arange(len(pos_outcome))
+    neg_idx = numpy.arange(len(neg_outcome))
+    random.shuffle(pos_idx)
+    random.shuffle(neg_idx)
+    i = 0
+    while not (len(validation) >= num):
+        validation.append(pos_outcome.iloc[pos_idx[i]])
+        validation.append(neg_outcome.iloc[neg_idx[i]])
+        i += 1
+    train = study[~study.patient_ID.isin(validation)]
+    validation = study[study.patient_ID.isin(validation)]
+    return train, validation
+
+
+def resample_patients_by_study(study_patients: Dict[str, list]) -> Dict[str, list]:
+    result = defaultdict(list)
+    max_length = max([len(x) for x in study_patients.values()])
+    for study, lst in study_patients.items():
+        result[study] += lst
+        to_upsample = max_length - len(lst)
+        result[study] += [random.choice(lst) for i in range(to_upsample)]
+    assert all([(len(x) == max_length) for x in result.values()])
+    return result
+
+
+def get_loc(patient_ID, frame):
+    return frame[frame.patient_ID == patient_ID].row_num.to_list()[0]
+
+
+def random_split(merged, bmc, feature_columns, label_columns, ratio=0.1, study_name=None, rand=False, to_numpy=True, balance_by_study=False):
+    """
+    Split dataset into train and validation sets:
+    --------------
+    Returns: train_data, train_labels, val_data, val_labels, expected
+        expected - confusion matrix expected from classification by ratio of positive/negative for each study
+    """
+    val_dict = defaultdict(list)
+    train_dict = defaultdict(list)
+    expected = dict()
+    expected['TN'] = 0
+    expected['FN'] = 0
+    expected['FP'] = 0
+    expected['TP'] = 0
+
+    for eval_study in set(bmc.study):
+        if study_name is not None:
+            if study_name != eval_study:
+                continue
+        study = bmc[bmc.study == eval_study]
+        num_select = math.ceil(len(study) * ratio)
+        study_patients = bmc[bmc.study == eval_study]
+        bmc_train, bmc_val = select_balanced_idx(study_patients, num_select)
+        pos_prob_train = bmc_train.posOutcome.sum() / len(bmc_train)
+        neg_prob_train = 1 - pos_prob_train
+        P = bmc_val.posOutcome.sum()
+        N = len(bmc_val) - P
+        TN = N * neg_prob_train
+        TP= P * pos_prob_train
+        FP = N - TN
+        FN = P - TP
+        expected['TN'] += TN
+        expected['TP'] += TP
+        expected['FP'] += FP
+        expected['FN'] += FN
+        val_dict[eval_study] = bmc_val.patient_ID.to_list()
+        train_dict[eval_study] = bmc_train.patient_ID.to_list()
+    if balance_by_study:
+        train_dict = resample_patients_by_study(train_dict)
+        iloc = []
+        for patient_lst in train_dict.values():
+            iloc += [get_loc(p, merged) for p in patient_lst]
+        train_split = merged.iloc[iloc]
+    else:
+        train_patients = []
+        for patient_lst in train_dict.values():
+            train_patients += patient_lst
+        train_split = merged[merged.patient_ID.isin(train_patients)]
+    val_patients = []
+    for patient_lst in val_dict.values():
+        val_patients += patient_lst
+    val_split = merged[merged.patient_ID.isin(val_patients)]
+    train_data = train_split[feature_columns]
+    train_labels = train_split[label_columns]
+    val_data = val_split[feature_columns]
+    val_labels = val_split[label_columns]
+    if rand:
+        train_data = numpy.random.randn(*train_data.shape)
+        val_data = numpy.random.randn(*val_data.shape)
+    if to_numpy:
+        train_data = train_data.to_numpy()
+        train_labels = train_labels.to_numpy().astype(int).ravel()
+        val_data = val_data.to_numpy()
+        val_labels = val_labels.to_numpy().astype(int).ravel()
+    return train_data, train_labels, val_data, val_labels, expected
+
+
+def digitize_genes(col, bins=15):
+    _, edges = numpy.histogram(col, bins=bins, density=True)
+    res = numpy.digitize(col, edges)
+    return res - res.min()
+
+
+def digitize_genes_by_median(col):
+    median = col.median()
+    return col > median
+
+
+res_ar = numpy.zeros((16, 4), dtype=numpy.bool_)
+for i in range(16):
+    ar0 = bin(i).split('0b')[1]
+    ar0 = '0' * (4 - len(ar0)) + ar0
+    res_ar[i] = [int(x) for x in ar0]
+
+
+
+def binary_genes(merged, by_median=True):
+    n_patients, n_genes = merged[genes_columns].shape
+    result = dict()
+    for gene_col in range(len(genes_columns)):
+    # for gene_col in range(1000):
+        if by_median:
+            digitized = digitize_genes_by_median(merged[genes_columns[gene_col]])
+            result[genes_columns[gene_col]] = digitized
+        else:
+            binary_genes = numpy.zeros((n_patients, 4), dtype=numpy.bool_)
+            digitized = digitize_genes(merged[genes_columns[gene_col]])
+            for i, digit in enumerate(digitized):
+                binary_genes[i] = res_ar[digit]
+            column_names = [genes_columns[gene_col] + '_{0}'.format(x) for x in range(4)]
+            for i, col in enumerate(column_names):
+                result[col] = binary_genes[:, i]
+    return result
+
+
+
+def digitize_non_genes_data(col):
+    not_nan = numpy.nan_to_num(col, nan=-1)
+    n_unique = len(set(not_nan))
+    if 15 < n_unique:
+        n_unique = 15
+    edge = numpy.histogram_bin_edges(numpy.nan_to_num(col, nan=col.min()), bins=n_unique - 1)
+    digits = numpy.digitize(not_nan, edge)
+    digits = digits - digits.min()
+    return digits
+
+def categorical_non_genes(merged, to_category=True):
+    non_genes_data = dict()
+    for oth in other_columns:
+        dig = digitize_non_genes_data(merged[oth])
+        ar1 = numpy.zeros(len(merged[oth]), dtype=object)
+        for i, d in enumerate(dig):
+            ar1[i] = string.ascii_letters[d]
+        non_genes_data[oth] = ar1
+    return non_genes_data
+
+
+def binary_non_genes(to_category=True):
+    non_genes_data = dict()
+    for oth in other_columns:
+        dig = digitize_non_genes_data(merged[oth])
+        set_size = len(set(dig))
+        new_col = -1
+        for x in range(1, 5):
+            if set_size <= 2 ** x:
+                new_col = x
+                break
+        assert new_col != -1
+        ar1 = numpy.zeros((len(merged[oth]), new_col), dtype=numpy.bool_)
+        for i, d in enumerate(dig):
+            ar1[i] = res_ar[d][-new_col:]
+        column_names = [oth + '_{0}'.format(x) for x in range(new_col)]
+        for i, col_name in enumerate(column_names):
+            non_genes_data[col_name] = ar1[:, i]
+    return non_genes_data
+
+def binarize_dataset(merged):
+    patients_id = merged.patient_ID.to_list()
+    binary_genes_dict = binary_genes(merged)
+    binary_non_genes_dict = categorical_non_genes()
+    binary_non_genes_dict['patient_ID'] = patients_id
+    binary_genes_dict['patient_ID'] = patients_id
+    binary_non_genes_dict['posOutcome'] = merged.posOutcome.to_list()
+    binary_genes_df = pandas.DataFrame(data=binary_genes_dict).sort_values(by='patient_ID') * 1
+    binary_non_genes_df = pandas.DataFrame(data=binary_non_genes_dict).sort_values(by='patient_ID')
+    result = pandas.merge(binary_genes_df, binary_non_genes_df, left_on='patient_ID', right_on='patient_ID').sort_values(by='patient_ID')
+    result.drop('patient_ID', axis=1, inplace=True)
+    return result
+
+
+def compute_metrics(result, y_true, y_pred, x_true, x_pred):
+    result['recall'].append(recall_score(y_true, y_pred))
+    result['precision'].append( precision_score(y_true, y_pred))
+    result['f1'].append(f1_score(y_true, y_pred))
+    result['confusion'].append(confusion_matrix(y_true, y_pred))
+    result['train_f1'].append(f1_score(x_true, x_pred))
+    result['train_confusion'].append(confusion_matrix(x_true, x_pred))
+    confusion = result['confusion'][-1]
+    accuracy = (confusion[0][0] + confusion[1][1]) / (sum(confusion[0]) + sum(confusion[1]))
+    result['accuracy'].append(accuracy)
